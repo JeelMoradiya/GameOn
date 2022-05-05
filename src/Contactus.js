7366769ae3e26efc9a947f5969af7d0a9034f48f
JeelMoaradiya <jeelmoradiya05@gmail.com>
2022-05-05 13:10:45 +0530

    Navbar

---
+++ src/Contactus.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Contactus = () =>{
+    return (<>
+        <h1>Welcome Contact US page</h1>
+        </>)
+}
+export default Contactus;
---
+++ src/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Login = () =>{
+    return (<>
+        <h1>Welcome login page</h1>
+        </>)
+}
+export default Login;
